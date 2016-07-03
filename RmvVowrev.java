1af6560c6f5b0b77fee3a89a797827ddcc0a6b3f
Kishorepm <kishorepmofficial@gmail.com>
2016-07-03 11:18:46 +0530

    Add files via upload

---
+++ RmvVowrev.java
@@ -0,0 +1,36 @@
+package player_LEVEL;
+
+import java.util.Scanner;
+
+public class RmvVowrev {
+	public static void main(String[]args)
+	{
+		String real;
+		Scanner a=new Scanner(System.in);
+		System.out.println("Enter the String:");
+		real=a.nextLine();
+		StringBuilder sb1 = new StringBuilder(real);
+	
+	  try{
+				for(int i=0;i<=real.length();i++)
+		
+			{
+				if(sb1.charAt(i)=='a' || sb1.charAt(i)=='e' || sb1.charAt(i)=='i' || sb1.charAt(i)=='o' || sb1.charAt(i)=='u' )
+				{
+					
+					sb1.deleteCharAt(i);
+					
+				}
+			}
+	
+	  }catch(Exception e ){
+			System.out.println("After vowels removed the reversed string is :"+sb1);
+	  }
+		StringBuffer sb=new StringBuffer(real);
+		sb.reverse();
+	    System.out.println("Reverse of entered string is: "+sb1);
+	   
+		
+	}
+	
+}
